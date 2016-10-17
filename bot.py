6431b04a967d31ecda64f8f0446194c64d36b92c
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2016-10-17 10:18:28 -0500

    Agregando la secuencia de la conversación y definiciend la opciones

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import pln
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 import os
-import re
-
 
 def listener(bot, update):
 	id = update.message.chat_id
@@ -11,37 +9,133 @@ def listener(bot, update):
 	#if message.lower() 
 	option = identifiedOptionConversation(id)
 	writeConversation(str(id),"user: "+message)
-	if option == 0: #se debe preguntar la edad de nuevo
+	if option == 0: #la respuesta hecha por el bot fue la edad
 		filterAge(message, bot, id)
 	else:
-		bot.sendMessage(chat_id=id, text="No entender")
-	
- #   if message.lower() :
- #   else:
- #       bot.sendMessage(chat_id=update.message.chat_id, text='No entiendo')
+		if option == 1:
+			filterProfesion(message,bot,id)
+		else:
+			if option == 2:
+				filterHourFood(message,bot,id)
+			else:
+				if option == 3:
+					filterTypeFood(message,bot,id)
+				else:
+					if option == 4:
+						filterCountriFood(message,bot,id)
+					else:
+						if option == 5:
+							filterCashOnly(message,bot,id)
+						else:
+							if option == 6:
+								filterDisability(message,bot,id)
+							else:
+								if option == 7:
+									filterParking(message,bot,id)
+								else:
+									if option == 8:
+										filterSmoking(message,bot,id)
+									else:
+										if option == 9:
+											filterAlcohol(message,bot,id)
+										else:
+											if option == 10:
+												filterWithFriends(message,bot,id)
+											else:
+												bot.sendMessage(chat_id=id, text="No entender")
+def filterWithFriends(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	bot.sendMessage(chat_id=id, text="Ha terminado la conversacion, te recomiendo")
+	writeConversation(str(id),"bot: "+message.lower())
+def filterAlcohol(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Vas con amigos?"
+	bot.sendMessage(chat_id=id, text="Y como última pregunta:")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+def filterSmoking(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Tienes pensado tomar alcohol hoy?"
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterParking(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Fumas?"
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterDisability(message,bot,id): #acesso para personas con discapacidad
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Necesitas estacionamiento?"
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterCashOnly(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Tienes alguna discapacidad o vas en compañía de alguien en esta condición?"
+	bot.sendMessage(chat_id=id, text="Para una mejor comodidad:")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterCountriFood(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Pagaras en efectivo o con algún tipo tarjeta?"
+	bot.sendMessage(chat_id=id, text="Unas preguntas más:")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterTypeFood(message,bot,id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Disfrutas más la comida de algún país en especial?"
+	bot.sendMessage(chat_id=id, text="¡Me antoje!")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterProfesion(message, bot, id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Quieres desayuno, almuerzo o cena?"
+	bot.sendMessage(chat_id=id, text="¡Muy buena profesión!, para tener más certeza:")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
+
+def filterHourFood(message, bot, id):
+	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
+	print (resultPln)
+	message="¿Cuál es el tipo de comida que más te gusta?"
+	bot.sendMessage(chat_id=id, text="Ahora dime:")
+	bot.sendMessage(chat_id=id, text=message)
+	writeConversation(str(id),"bot: "+message.lower())
 
 def readLastMessageConversation(id): #retornar la ultima linea del archivo que tiene creado para el usuario
 	return readLastLineConversation(str(id))[len(readLastLineConversation(str(id)))-1].lower().replace('bot: ','').replace(' ','').replace('\n','')
 
-def identifiedOptionConversation(id): #retorna el identificador para cada 
-	lastMessage=readLastMessageConversation(id)
-	print(lastMessage)
-	if lastMessage=='¿quéedadtienes?':
-		return 0
-	else:
-		return 1
+
 
 def isValidateAge(age):
 	return True if age>=15 and age<=100 else False
 
 def filterAge(message, bot, id):
-	patron = re.compile('(?:\d*)?\d+')
 	resultPln = pln.filterSignes(list(pln.clearEmptyWords(pln.separateText(message))))
 	if(isOneDigit(resultPln)):
 		value = int(resultPln[0])
 		if(isValidateAge(value)):#es un rango de edad valido?
 			#se debe asginar el valor difurso con este valor que es un solo digito     
-			print('valor difuso para: '+str(value))
+			print('falta hallar valor difuso para: '+str(value))
+			#se continua con la conversación porque ya tomo el valor difuso
+			message="¿Cuál es tu profesión o qué haces a diario?"
+			bot.sendMessage(chat_id=id, text="¡Que bien! y cuentame:")
+			bot.sendMessage(chat_id=id, text=message)
+			writeConversation(str(id),"bot: "+message.lower())
 		else:
 			message="¿Qué edad tienes?"
 			bot.sendMessage(chat_id=id, text="¡Ops, tu edad no se encuentra en el rango de 15-100 años, ingresa tu edad correctamente!")
@@ -56,11 +150,20 @@ def filterAge(message, bot, id):
 				bot.sendMessage(chat_id=id, text=message)
 				writeConversation(str(id),"bot: "+message.lower())
 			else:#se extrae el primero valor digito de todos los texto
-				print('digito encontrado')
-				print(firstDigit(resultPln))
+				value = int(firstDigit(resultPln))
+				print('falta hallar valor difuso para: '+str(value))
+				#se continua con la conversación porque ya tomo el valor difuso
+				message="¿Cuál es tu profesión o qué haces a diario?"
+				bot.sendMessage(chat_id=id, text="¡Que bien! y cuentame:")
+				bot.sendMessage(chat_id=id, text=message)
+				writeConversation(str(id),"bot: "+message.lower())
 		else: 
 			#no se esta reconociendo ningun digito pero esta palabra paso el pln
-			print(resultPln)
+			#print(resultPln)
+			message="¿Qué edad tienes?"
+			bot.sendMessage(chat_id=id, text="¡Ops, lo siento pero aún no se reconoce la fecha por texto, ingresa tu edad como un número!")
+			bot.sendMessage(chat_id=id, text=message)
+			writeConversation(str(id),"bot: "+message.lower())
 
 def firstDigit(resultPln):
 	for word in resultPln:
@@ -68,10 +171,10 @@ def firstDigit(resultPln):
 			return word
 	return 'None'
 def isOneDigit(resultPln):
-	return (len(findDigits(resultPln)) ==1 and len(resultPln) == 1) 
+	return len(findDigits(resultPln)) ==1 and len(resultPln) == 1
 
 def isOnlyDigits(resultPln):
-	return (len(findDigits(resultPln)) ==len(resultPln)) 
+	return len(findDigits(resultPln))==len(resultPln)
 
 def findDigits(resultPln):
 	words = []
@@ -98,6 +201,44 @@ def readLastLineConversation(id):
 	data = file.readlines()
 	file.close()
 	return data
+
+def identifiedOptionConversation(id): #retorna el identificador para cada 
+	lastMessage=readLastMessageConversation(id)
+	print(lastMessage)
+	if lastMessage=='¿quéedadtienes?':
+		return 0
+	else:
+		if lastMessage =='¿cuálestuprofesiónoquéhacesadiario?':
+			return 1
+		else:
+			if lastMessage =='¿quieresdesayuno,almuerzoocena?':
+				return 2
+			else:
+				if lastMessage =='¿cuáleseltipodecomidaquemástegusta?':
+					return 3
+				else:
+					if lastMessage =='¿disfrutasmáslacomidadealgúnpaísenespecial?':
+						return 4
+					else:
+						if lastMessage =='¿pagarasenefectivooconalgúntipotarjeta?':
+							return 5
+						else:
+							if lastMessage =='¿tienesalgunadiscapacidadovasencompañíadealguienenestacondición?':
+								return 6
+							else:
+								if lastMessage =='¿necesitasestacionamiento?':
+									return 7
+								else:
+									if lastMessage =='¿fumas?':
+										return 8
+									else:
+										if lastMessage =='¿tienespensadotomaralcoholhoy?':
+											return 9
+										else:
+											if lastMessage =='¿vasconamigos?':
+												return 10
+											else:
+												return 11	
 
 def main():
 	updater = Updater('205961193:AAE4XZ9K6VcdfnzM7DvTuI1JsIF_SVKQ_Fo')
